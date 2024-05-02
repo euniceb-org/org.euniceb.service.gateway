6b814e01619ca4879faa0067400641b5cd785386
Eunice B <eunice@reach.africa>
2024-05-02 13:03:58 +0100

    Created gateway service for euniceb.org

---
+++ src/test/java/org/euniceb/service/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.euniceb.service.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
